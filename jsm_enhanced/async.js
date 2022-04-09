5f624c48666146b08fecb4d851e0ee0fe2e0b594
mart3323 <Mart3323@gmail.com>
2022-04-09 11:22:41 +0300

    Add some comments

--- jsm_enhanced/async.js
+++ jsm_enhanced/async.js
@@ -4,22 +4,30 @@ const {proxify} = require("./utils.js");
 /*
   Concurrency-friendly waitTick() replacement
 */
+/** Local tick counter */
 let tickRel = 0
+/** Map of tick offsets to functions that need to be called at that offset */
 const tickMap = {}
 
 JsMacros.on('Tick', JavaWrapper.methodToJava(() => {
-  tickRel += 1;
+  // Count up by one every tick
+  tickRel += 1; 
+  // If the current offset has functions to call, call them all
   if (tickMap[tickRel]) {
     tickMap[tickRel].forEach(fn => {
       fn()
     })
   }
+  // Now that we're past the offset, delete the callbacks if there were any
   delete tickMap[tickRel]
 
+  // Reset the counter to 0 when nothing is pending because why not
   if (Object.keys(tickMap).length === 0) {
     tickRel = 0;
   }
 }))
+
+// Actual implementation called by the user - just returns a promise and adds the resolve function to the tickmap to be called n ticks in the future
 const waitTick = (n = 1) => new Promise(async res => {
   if (tickMap[tickRel + n]) {
     tickMap[tickRel + n].push(res)
@@ -43,6 +51,8 @@ const RESOLUTION = 10;
 const yieldExecution = () => new Promise(res =>
   JavaWrapper.methodToJavaAsync(res).run()
 )
+
+// Similar to tickMap - map of ms offsets into functions that need to be called at that offset
 const sleepMap = {}
 let runningSleep = false
 
@@ -53,26 +63,32 @@ const sleep = (n) => new Promise(async res => {
   }
   sleepMap[targetTime].push(res)
 
+  // Since this one is implemented with blocking, we need to ensure only one copy runs
   if (runningSleep) return
   runningSleep = true
 
   while (Object.keys(sleepMap).length) {
+    // Take the smallest currently remaining delay
     const [next] = Object.keys(sleepMap).map(Number).sort()
     const now = JSMTime.time();
     const delta = next - now;
     if (delta < 0) {
+      // If we're already past the intended time, call the functions immediately
       sleepMap[next].forEach(fn => fn())
       delete sleepMap[next]
     } else if (delta < RESOLUTION) {
+      // If we're close to the intended time, sleep until that time and then call the functions immediately
       JSMTime.sleep(delta);
       sleepMap[next].forEach(fn => fn())
       delete sleepMap[next]
     } else {
+      // Else sleep for RESOLUTION and recheck the map in case other sleeps have been added in the meantime
       JSMTime.sleep(RESOLUTION)
     }
+    // Allow events / waitTIck a chance to run - this happens at least every RESOLUTION ms
     await yieldExecution()
   }
-
+  // Once there is nothing to wait for we can stop this loop
   runningSleep = false
 });
 
